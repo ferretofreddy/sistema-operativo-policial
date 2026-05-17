ec7fd55c5b2aca48ae39b5cb30a4a6f361e6077b
Valiroi <ferretofreddy@hotmail.com>
2026-05-17 21:19:09 +0000

    gitpod ajustes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      open: true,
-      gzipSize: true,
-    }),
-  ],
+  plugins: [react()],
+
   server: {
-    host: '0.0.0.0',
-    port: 5173
+    host: "0.0.0.0",
+    port: 5173,
+
+    strictPort: true,
+
+    allowedHosts: [
+      ".gitpod.dev",
+      ".gitpod.io",
+    ],
+
+    hmr: {
+      clientPort: 443,
+    },
   },
 });
-
-
-
