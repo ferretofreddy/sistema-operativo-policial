61017c4027d67f0c93edc362155f4deafe3a8158
Valiroi <ferretofreddy@hotmail.com>
2026-05-20 12:58:40 -0600

    Refactor main.jsx to simplify component structure

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,14 @@
 // frontend/src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter } from "react-router-dom";
+
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
